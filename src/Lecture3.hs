c4b6b439f5a641fbf7118a12c4a08ab5411977d6
Julian Batongbacal <julian.batongbacal@gmail.com>
2022-02-28 11:32:01 +1100

    toshortstring and next

--- src/Lecture3.hs
+++ src/Lecture3.hs
@@ -49,7 +49,7 @@ data Weekday
     | Friday
     | Saturday
     | Sunday
-    deriving (Show, Eq)
+    deriving (Show, Eq, Enum, Bounded)
 
 {- | Write a function that will display only the first three letters
 of a weekday.
@@ -57,7 +57,8 @@ of a weekday.
 >>> toShortString Monday
 "Mon"
 -}
-toShortString = error "TODO"
+toShortString :: Show a => a -> String
+toShortString = take 3 . show
 
 {- | Write a function that returns next day of the week, following the
 given day.
@@ -79,7 +80,10 @@ Tuesday
   would work for **any** enumeration type in Haskell (e.g. 'Bool',
   'Ordering') and not just 'Weekday'?
 -}
-next = error "TODO"
+next :: Weekday -> Weekday
+next x
+    | x == maxBound     = minBound 
+    | otherwise         = succ x
 
 {- | Implement a function that calculates number of days from the first
 weekday to the second.
